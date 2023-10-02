f86e181282e536c8c01fa8e9d5f1116d8925f999
seppe-ado <seppe.ado.vandewalle@gmail.com>
2023-10-02 18:03:50 +0200

    Add project files.

---
+++ Boek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced.FirstAssignment
+{
+    internal class Boek
+    {
+
+        public int Isbn { get; set; }
+        public string Naam { get; set; }
+        public string Uitgever { get; set; }
+        public double Prijs { get; set; }
+
+
+        public override string ToString()
+        {
+            return (Isbn + ", " + Naam + ", " + Uitgever + ", " + Prijs);
+        }
+
+        public void Lees(Boek boek,int isbn,string naam, string uitgever,double prijs)
+        {
+            boek.Isbn = isbn;
+            boek.Naam = naam;
+            boek.Uitgever = uitgever;
+            boek.Prijs = prijs; 
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Advanced.FirstAssignment;
+
+Boek boek = new Boek();
+Boek boek2 = new Boek();
+
+boek.Lees(boek, 1, "naam1", "Uitgever1", 10);
+boek2.Lees(boek2, 1, "naam2", "Uitgever2", 10);
+
+
+Console.WriteLine(boek.ToString());
+Console.WriteLine(boek2.ToString());
